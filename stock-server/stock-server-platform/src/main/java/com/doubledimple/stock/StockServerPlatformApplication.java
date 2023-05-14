b216134c9f92b030f9c86805a4d4fdc0c39b4f26
doubleDimple <lovele.cn@gmail.com>
2023-05-14 22:08:08 +0800

    dubbo

--- stock-server/stock-server-platform/src/main/java/com/doubledimple/stock/StockServerPlatformApplication.java
+++ stock-server/stock-server-platform/src/main/java/com/doubledimple/stock/StockServerPlatformApplication.java
@@ -4,12 +4,9 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.doubledimple.stock.mapper")
-@EnableDiscoveryClient
-@EnableDubbo
 public class StockServerPlatformApplication {
 
     public static void main(String[] args) {
